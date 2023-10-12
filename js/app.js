3d1cfb6491fe6ebdc200ef061c482fd37109e3e0
SandarvaShakya <sandarvashakya34@gmail.com>
2023-10-13 00:06:42 +0545

    Add movements to the board

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ const ROWS = 4;
 const COLUMNS = 4;
 
 // Scores
-const SCORE = 0;
-const HIGH_SCORE = 0;
+let SCORE = 0;
+let HIGH_SCORE = 0;
 
 const SCORE_ELEMENT = document.getElementById("score");
 const HIGH_SCORE_ELEMENT = document.getElementById("high-score");
@@ -15,24 +15,25 @@ const BOARD = document.getElementById("board");
 document.addEventListener("keydown", (event) => {
 	switch (event.key) {
 		case "ArrowRight":
-			console.log("Slide right");
-			break;
-		case "a":
-			console.log("Slide right");
-			break;
-		case "ArrowLeft":
-			console.log("Slide left");
+			moveRight();
 			break;
 		case "d":
-			console.log("Slide left");
+			moveRight();
+			break;
+		case "ArrowLeft":
+			moveLeft();
+			break;
+		case "a":
+			moveLeft();
 			break;
 		case "ArrowUp":
-			console.log("Slide up");
+			moveUp();
 			break;
 		case "w":
-			console.log("Slide up");
+			moveUp();
 			break;
 		case "ArrowDown":
+			moveDown();
 			console.log("Slide down");
 			break;
 		case "s":
@@ -41,11 +42,13 @@ document.addEventListener("keydown", (event) => {
 	}
 });
 
+let BOARD_ARRAY;
+
 window.onload = () => {
-	const BOARD_ARRAY = [
-		[2, 2, 2, 2],
-		[2, 2, 4, 8],
-		[4, 4, 4, 16],
+	BOARD_ARRAY = [
+		[2, 2, 2, 0],
+		[2, 2, 4, 0],
+		[4, 0, 0, 16],
 		[4, 4, 4, 16],
 	];
 	// const BOARD_ARRAY = [
@@ -57,7 +60,7 @@ window.onload = () => {
 
 	for (let row = 0; row < ROWS; row++) {
 		for (let column = 0; column < COLUMNS; column++) {
-			const TILE = createTile();
+			const TILE = createTile(row, column);
 			TILE.classList.add("tile");
 			const TILE_VALUE = BOARD_ARRAY[row][column];
 			updateTile(TILE, TILE_VALUE);
@@ -66,16 +69,113 @@ window.onload = () => {
 	}
 };
 
-const createTile = () => {
-	return document.createElement("div");
+const createTile = (row, column) => {
+	const TILE = document.createElement("div");
+	TILE.id = `${row}-${column}`;
+	return TILE;
 };
 
 const updateTile = (tile, tileValue) => {
 	tile.innerText = "";
-
+	tile.classList = "";
+	tile.classList.add("tile");
 	if (tileValue > 0) {
 		tile.innerText = tileValue.toString();
 		if (tileValue <= 4096) tile.classList.add(`color-${tileValue}`);
 		else tile.classList.add(`color-4096`);
+	}
+};
+
+const createAndUpdateTile = (row, board_array) => {
+	for (let column = 0; column < COLUMNS; column++) {
+		const TILE = document.getElementById(`${row}-${column}`);
+		updateTile(TILE, BOARD_ARRAY[row][column]);
+	}
+};
+
+const removeZeros = (array) => {
+	return array.filter((item) => {
+		return item !== 0;
+	});
+};
+
+const addZeros = (array) => {
+	while (array.length < COLUMNS) {
+		array.push(0);
+	}
+
+	return array;
+};
+
+const mergeEachRow = (board_row) => {
+	board_row = removeZeros(board_row);
+
+	for (let i = 0; i < board_row.length - 1; i++) {
+		if (board_row[i] === board_row[i + 1]) {
+			board_row[i] *= 2;
+			board_row[i + 1] = 0;
+			SCORE += board_row[i];
+		}
+		board_row = removeZeros(board_row);
+	}
+	board_row = addZeros(board_row);
+	return board_row;
+};
+
+const moveLeft = () => {
+	for (let row = 0; row < ROWS; row++) {
+		let board_row = BOARD_ARRAY[row];
+		board_row = mergeEachRow(board_row);
+		BOARD_ARRAY[row] = board_row;
+		createAndUpdateTile(row, BOARD_ARRAY);
+	}
+};
+
+const moveRight = () => {
+	for (let row = 0; row < ROWS; row++) {
+		let board_row = BOARD_ARRAY[row];
+		board_row.reverse();
+		board_row = mergeEachRow(board_row);
+		board_row.reverse();
+		BOARD_ARRAY[row] = board_row;
+		createAndUpdateTile(row, BOARD_ARRAY);
+	}
+};
+
+function transpose(board) {
+	return board[0].map((col, i) => {
+		return board.map((row) => {
+			return row[i];
+		});
+	});
+}
+
+const moveUp = () => {
+	let transposedBoard = transpose(BOARD_ARRAY);
+	for (let row = 0; row < ROWS; row++) {
+		let board_row = transposedBoard[row];
+		board_row = mergeEachRow(board_row);
+		transposedBoard[row] = board_row;
+	}
+	BOARD_ARRAY = transpose(transposedBoard);
+
+	for (let row = 0; row < ROWS; row++) {
+		createAndUpdateTile(row, BOARD_ARRAY);
+	}
+};
+
+const moveDown = () => {
+	let transposedBoard = transpose(BOARD_ARRAY);
+	for (let row = 0; row < ROWS; row++) {
+		let board_row = transposedBoard[row];
+		board_row.reverse();
+		board_row = mergeEachRow(board_row);
+		board_row.reverse();
+		transposedBoard[row] = board_row;
+	}
+	BOARD_ARRAY = transpose(transposedBoard);
+
+	for (let row = 0; row < ROWS; row++) {
+		createAndUpdateTile(row, BOARD_ARRAY);
 	}
 };
